77fd761e76ad1070088d074e0737bf9551270065
madmike 🧙 <michaelgunnulfsen@gmail.com>
2021-07-19 20:49:48 +0200

    add channel uri

--- podcasts/fetch/fetchChannel.js
+++ podcasts/fetch/fetchChannel.js
@@ -1,34 +1,34 @@
-const get = require('lodash.get')
-const { feedXmlToEpisode } = require('./mappers')
-const { fetchAndParse, isValidEpisode } = require('./utils')
+const get = require("lodash.get");
+const { feedXmlToEpisode } = require("./mappers");
+const { fetchAndParse, isValidEpisode } = require("./utils");
 
 const fetchChannel = async (uri) => {
-    try {
-        console.log(`fetchChannel() ${uri}`)
-        const parsed = await fetchAndParse(uri)
-        const episodes = parsed.items
-            .map((epi) => feedXmlToEpisode(parsed, epi))
-            .filter(({ data: { imageUri, title, episodeUri }, channelTitle }) =>
-                isValidEpisode({ imageUri, title, channelTitle, episodeUri })
-            )
+  try {
+    console.log(`fetchChannel() ${uri}`);
+    const parsed = await fetchAndParse(uri);
+    const episodes = parsed.items
+      .map((epi) => feedXmlToEpisode(parsed, epi, uri))
+      .filter(({ data: { imageUri, title, episodeUri }, channelTitle }) =>
+        isValidEpisode({ imageUri, title, channelTitle, episodeUri })
+      );
 
-        return {
-            uri,
-            episodes,
-            meta: {
-                title: parsed.title,
-                publisherName: get(parsed, 'itunes.owner.name'),
-                description: parsed.description,
-                image: parsed.image.url
-            }
-        }
-    } catch (error) {
-        console.log(`Fetch channel error: ${uri}`)
-        console.log(error)
-        return null
-    }
-}
+    return {
+      uri,
+      episodes,
+      meta: {
+        title: parsed.title,
+        publisherName: get(parsed, "itunes.owner.name"),
+        description: parsed.description,
+        image: parsed.image.url,
+      },
+    };
+  } catch (error) {
+    console.log(`Fetch channel error: ${uri}`);
+    console.log(error);
+    return null;
+  }
+};
 
 module.exports = {
-    fetchChannel
-}
+  fetchChannel,
+};
--- podcasts/fetch/mappers.js
+++ podcasts/fetch/mappers.js
@@ -1,60 +1,68 @@
-const get = require('lodash.get')
-const striptags = require('striptags')
-const { formatDuration } = require('./timeStringifier')
+const get = require("lodash.get");
+const striptags = require("striptags");
+const { formatDuration } = require("./timeStringifier");
 
-const feedXmlToEpisode = (xmlChannel, epi) => {
-    const channelImageUri = get(xmlChannel, 'image.url') || get(xmlChannel, 'itunes.image')
+const feedXmlToEpisode = (xmlChannel, epi, uri) => {
+  const channelImageUri =
+    get(xmlChannel, "image.url") || get(xmlChannel, "itunes.image");
 
-    const duration = formatDuration(get(epi, 'itunes.duration')) || ''
-    const episode = {
-        channelImageUri,
-        channelTitle: xmlChannel.title,
-        data: {
-            duration,
-            episodeUri: get(epi, 'enclosures[0].url'),
-            title: epi.title,
-            description: getEpisodeDescription(epi),
-            // @ts-ignore
-            imageUri: get(epi, 'itunes.image') || channelImageUri,
-            published: epi.published,
-            summary: getEpisodeSummary(epi),
-            id: epi.id || epi.guid
-        }
-    }
+  const duration = formatDuration(get(epi, "itunes.duration")) || "";
+  const episode = {
+    channelImageUri,
+    channelTitle: xmlChannel.title,
+    channelUri: uri,
+    data: {
+      duration,
+      episodeUri: get(epi, "enclosures[0].url"),
+      title: epi.title,
+      description: getEpisodeDescription(epi),
+      // @ts-ignore
+      imageUri: get(epi, "itunes.image") || channelImageUri,
+      published: epi.published,
+      summary: getEpisodeSummary(epi),
+      id: epi.id || epi.guid,
+    },
+  };
 
-    return episode
-}
+  return episode;
+};
 
 const omitDescription = (epi) => {
-    return {
-        ...epi,
-        data: {
-            ...epi.data,
-            description: ''
-        }
-    }
-}
+  return {
+    ...epi,
+    data: {
+      ...epi.data,
+      description: "",
+    },
+  };
+};
 
 const omitDescriptionFromDbEpisode = (epi) => {
-    return {
-        ...epi,
-        description: ''
-    }
-}
+  return {
+    ...epi,
+    description: "",
+  };
+};
 
 const getEpisodeDescription = (episode) => {
-    const description = episode.description || ''
-    const itunesSubTitle = get(episode, 'itunes.subtitle') || ''
+  const description = episode.description || "";
+  const itunesSubTitle = get(episode, "itunes.subtitle") || "";
 
-    return description.length > itunesSubTitle.length ? description : itunesSubTitle
-}
+  return description.length > itunesSubTitle.length
+    ? description
+    : itunesSubTitle;
+};
 
-const MAX_SIZE_SUMMARY = 255
+const MAX_SIZE_SUMMARY = 255;
 const getEpisodeSummary = (epi) => {
-    const str = get(epi, 'itunes.subtitle') || get(epi, 'itunes.summary') || epi.description || ''
-    return striptags(str).substr(0, MAX_SIZE_SUMMARY)
-}
+  const str =
+    get(epi, "itunes.subtitle") ||
+    get(epi, "itunes.summary") ||
+    epi.description ||
+    "";
+  return striptags(str).substr(0, MAX_SIZE_SUMMARY);
+};
 
 module.exports = {
-    feedXmlToEpisode
-}
+  feedXmlToEpisode,
+};
